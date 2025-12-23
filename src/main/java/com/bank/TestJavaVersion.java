d48ccf464ac3b0c92ec82bfda729738f132ce07d
Ilham <ilham.mhari.ESTN24@ump.ac.ma>
2025-12-23 20:22:48 +0100

    Update4

--- src/main/java/com/bank/TestJavaVersion.java
+++
@@ -1,8 +0,0 @@
-package com.bank;
-
-public class TestJavaVersion {
-    public static void main(String[] args) {
-        System.out.println("Java version: " + System.getProperty("java.version"));
-        System.out.println("Java configuré correctement !");
-    }
-}
